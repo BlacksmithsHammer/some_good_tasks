684daa416351452f25e863d923e583b898fd1160
Fatyushkin D.V <99551946+BlacksmithsHammer@users.noreply.github.com>
2024-02-14 18:27:28 +0300

    Update serializer.sv attempt 2

--- fpga-tasks/serializer/rtl/serializer.sv
+++ fpga-tasks/serializer/rtl/serializer.sv
@@ -1,9 +1,10 @@
 module serializer (
   input  logic         clk_i,
   input  logic         srst_i,
+
   input  logic [15:0]  data_i,
-  input  logic         data_val_i,
   input  logic [3:0]   data_mod_i,
+  input  logic         data_val_i,
 
   output logic         ser_data_o,
   output logic         ser_data_val_o,
@@ -14,48 +15,49 @@ module serializer (
   logic [3:0]  cnt;
   logic        ser_data_val;
   logic [15:0] data;
+  logic        flag;
+
+  assign flag = data_val_i && ~busy && ( data_mod_i > 2 || data_mod_i == 0);
 
   always_ff @( posedge clk_i )
-    begin
-      if( srst_i )
-        begin
-          ser_data_val <= 1'b0;
-          busy         <= 1'b0;
-          data         <= '0;
-          cnt          <= '0;
-        end
+    if( srst_i )
+      busy <= 1'b0;
+    else
+      if( flag )
+        busy <= 1'b1;
       else
-        if( data_val_i && ~busy && ( data_mod_i > 2 || data_mod_i == 0 ) )
-          begin
-            busy         <= 1'b1;
-            ser_data_val <= 1'b1;
-            data         <= data_i;
-            cnt          <= ( data_mod_i - 1 );
-          end
-        else
-          if (~busy && data_mod_i < 3)
-            ser_data_val <= 1'b0;
+        if (cnt < 2)
+          busy <= 1'b0;
+      
+  always_ff @( posedge clk_i )
+    if( srst_i )
+      ser_data_val <= 1'b0;
+    else
+      if( flag )
+        ser_data_val <= 1'b1;
+      else
+        if( ~busy )
+          ser_data_val <= 1'b0;
 
-      if ( busy )
-        begin
-          if ( cnt > 1 )
-            begin
-              for (int i = 15; i > 0; i = i - 1)
-                data[i] <= data[i-1];
-              data[0] <= 1'b0;
-              cnt     <= cnt - 1;
-            end
-          else
-            begin
-              cnt      <= cnt - 1;
-              data[15] <= data[14];
-              busy     <= 1'b0;
-            end
-        end
+  always_ff @( posedge clk_i )
+    if( srst_i )
+      cnt <= '0;
+    else
+      if( flag )
+        cnt <= (data_mod_i - 4'b1);
       else
-        if ( ~data_val_i)
-          ser_data_val <= 1'b0;
-    end
+        if( cnt > 0 )
+          cnt <= cnt - 1;
+
+  always_ff @( posedge clk_i )
+    if( srst_i )
+      data <= '0;
+    else
+      if( flag )
+        data <= data_i;
+      else
+        if( cnt > 0 )
+          data <= data << 1;
 
   assign ser_data_o     = data[15];
   assign busy_o         = busy;
