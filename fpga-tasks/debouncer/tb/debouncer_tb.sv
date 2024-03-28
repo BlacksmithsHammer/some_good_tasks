a020fbcab15d0e7fc6afae5f2d27f0c02ae39ada
Fatyushkin D.V <99551946+BlacksmithsHammer@users.noreply.github.com>
2024-03-28 18:24:32 +0300

    add "bouncing" case of key_i to test

--- fpga-tasks/debouncer/tb/debouncer_tb.sv
+++ fpga-tasks/debouncer/tb/debouncer_tb.sv
@@ -44,7 +44,7 @@ int  num_cycles;
 initial 
   begin
     key = 1'b1;
-    for(int test_num = 0; test_num < 100000; test_num = test_num + 1)
+    for(int test_num = 0; test_num < 1000; test_num = test_num + 1)
       begin
         //if GLITCH more than 10000ns - should use an other way to set range
         num_cycles = $urandom_range(GLITCH_TIME_NS, 1);
@@ -81,7 +81,7 @@ initial
 
 
     key = 1'b1;
-    for(int test_num = 0; test_num < 100000; test_num = test_num + 1)
+    for(int test_num = 0; test_num < 1000; test_num = test_num + 1)
       begin
 
         //if GLITCH more than 10000ns - should use an other way to set range
@@ -131,10 +131,58 @@ initial
                 end
             ##1;
           end
-        
       end
 
+    key = 1'b1;
+    for(int test_num = 0; test_num < 1000; test_num = test_num + 1)
+      begin
+        num_cycles = $urandom_range(GLITCH_TIME_CYCLES, 1);
+        // + 3 because DFF-delay of resync key_i
+        if( num_cycles >= GLITCH_TIME_CYCLES )
+          expected_curr_stb = $time + TIME_OF_CYCLE*(GLITCH_TIME_CYCLES + 3);
+        else
+          expected_curr_stb = '0;
 
+        for(int test_cycle = 0; test_cycle < num_cycles; test_cycle = test_cycle + 1)
+          begin
+            key = 1'b0;
+            if( key_pressed_stb == 1'b1 && $time != expected_curr_stb && $time != expected_prev_stb && $time != expected_prev_prev_stb )
+              begin
+                $display("WRONG STB AT ", $time);
+                $display("expected stb's at: ", expected_curr_stb, "or", expected_prev_stb, " or ", expected_prev_prev_stb);
+                ##3;
+                $stop();
+              end
+            if( key_pressed_stb == 1'b0 )
+              if( $time == expected_curr_stb == 1'b1 || $time == expected_prev_stb || $time == expected_prev_prev_stb )
+                begin
+                  $display("EXPECTED STB BUT NO AT ", $time);
+                  $stop();
+                end
+            ##1;
+          end
+        key = 1'b1;
+        expected_prev_prev_stb = expected_prev_stb;
+        expected_prev_stb      = expected_curr_stb;
+        //can randomize pause to deep testing
+        for(int unpush = 0; unpush < $urandom_range(GLITCH_TIME_CYCLES, 1); unpush = unpush + 1)
+          begin
+            if( key_pressed_stb == 1'b1 && $time != expected_curr_stb && $time != expected_prev_stb && $time != expected_prev_prev_stb )
+              begin
+                $display("WRONG STB AT ", $time);
+                $display("expected stb's at: ", expected_curr_stb, "or", expected_prev_stb, " or ", expected_prev_prev_stb);
+                ##3;
+                $stop();
+              end
+            if( key_pressed_stb == 1'b0 )
+              if( $time == expected_curr_stb == 1'b1 || $time == expected_prev_stb || $time == expected_prev_prev_stb )
+                begin
+                  $display("EXPECTED STB BUT NO AT ", $time);
+                  $stop();
+                end
+            ##1;
+          end
+      end
 
 
     $display("============================\nTESTS PASSED SUCCESSFULLY\n============================\n");
