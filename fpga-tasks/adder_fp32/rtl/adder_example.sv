ced9e57e11a1ca32a110613e9372f7f92dc4817a
Fatyushkin D.V <99551946+BlacksmithsHammer@users.noreply.github.com>
2024-05-19 17:04:44 +0300

    Added TB and good interface to make custom tests

--- fpga-tasks/adder_fp32/rtl/adder_example.sv
+++ fpga-tasks/adder_fp32/rtl/adder_example.sv
@@ -2,15 +2,40 @@ module adder_example (
     input  clk,
     input  srst,
 
-    input  valid_stb_i,
     output ready_o,
+
+    output valid_stb_o,
+    input  valid_stb_i,
 
     input  [31:0] a_i,
     input  [31:0] b_i,
 
-    output [31:0] z_o
-);
+    output [31:0] z_o,
+    input         ack_z_i
 
+);
+  ////////////////////////////////////////////////////////
+  //custom addition for easier use
+  //now we can send valid data once (in 1 clock)
+  //obviously not best solution, but this move only for make better interface
+
+  logic [31:0]  b;
+  logic [1:0]   b_d;
+
+  always_ff @( posedge clk ) 
+    if( srst )
+      b_d <= '0;
+    else
+      b_d <= {b_d[0], valid_stb_i};
+        
+
+  always_ff @( posedge clk )
+    if( valid_stb_i )
+      b <= b_i;
+
+  ////////////////////////////////////////////////////////
+
+ 
   //https://github.com/dawsonjon/fpu/tree/master/adder
   adder adder_ins (
     .clk          ( clk  ),
@@ -20,13 +45,16 @@ module adder_example (
     .input_a_stb  ( valid_stb_i ),
     .input_a_ack  ( ready_o     ),
 
-    .input_b      ( b_i ),
-    .input_b_stb  ( valid_stb_i ),
+    .input_b      ( b      ),
+    .input_b_stb  ( b_d[1] ),
+
+  //ignored because number of cycles to get b value fixed
     .input_b_ack  (),
 
-    .output_z     ( z_o ),
-    .output_z_stb (),
-    .output_z_ack ()
+    .output_z     ( z_o         ),
+    .output_z_stb ( valid_stb_o ),
+    .output_z_ack ( ack_z_i     )
   );
     
 endmodule
+
--- fpga-tasks/adder_fp32/tb/adder_example_tb.sv
+++ fpga-tasks/adder_fp32/tb/adder_example_tb.sv
@@ -3,11 +3,16 @@ module adder_example_tb #();
   bit clk;
   bit srst;
 
+  logic         ready;
+
   logic         valid_stb_input;
   logic  [31:0] a;
   logic  [31:0] b;
 
+  logic         ack_z;
   logic  [31:0] z;
+  logic         valid_stb_z;
+
 
   initial
     forever
@@ -18,32 +23,86 @@ module adder_example_tb #();
   endclocking
 
   adder_example adder_ex_ins (
-    .clk  (clk),
-    .srst (srst),
+    .clk  ( clk  ),
+    .srst ( srst ),
 
-    .valid_stb_i (valid_stb_input),
-    .a_i  (a),
-    .b_i  (b),
+    .valid_stb_i ( valid_stb_input),
+    .ready_o     ( ready          ),
 
-    .z_o  (z)
+    .a_i         ( a              ),
+    .b_i         ( b              ),
+
+    //signal of valid z = a + b in output
+    .valid_stb_o ( valid_stb_z    ),
+    .z_o         ( z              ),
+    .ack_z_i     ( ack_z          )
   );
+
+  task send_a_b(logic [31:0] test_a,
+                logic [31:0] test_b);
+    valid_stb_input = 1'b1;
+    a = test_a;
+    b = test_b;
+    ##1;
+    valid_stb_input = 1'b0;
+  endtask
+  
+
+  int counter_test = 0;
+  task new_test(logic [31:0] test_a,
+                logic [31:0] test_b,
+                logic [31:0] test_expected);
+    wait(ready);
+    ##1;
+    send_a_b(test_a, test_b);
+    wait(valid_stb_z);
+    ack_z = 1'b1;
+    counter_test++;
+    $display("=========================Test case %d =========================", counter_test);
+    $display("%f + %f == %f", $bitstoshortreal(test_a), 
+                              $bitstoshortreal(test_b), 
+                              $bitstoshortreal(z));
+    ##2;
+    ack_z = 1'b0;
+    if( z != test_expected )
+      begin
+        $display("test %d not passed, expected: %f,  got: ", counter_test,
+                                                    $bitstoshortreal(test_expected),
+                                                    $bitstoshortreal(z));
+        $stop();
+      end
+  endtask
 
   initial
     begin
+      //initialization
+      ack_z = 1'b0;
+      valid_stb_input = 1'b0;
       srst = 1'b1;
       ##1;
       srst = 1'b0;
-      ##5;
-      valid_stb_input = 1'b1;
-      a = 32'b01000010100000001101110100101111; //64.432
-      b = 32'b01000011010000011110011001100110; //193.9
-      ##1;
-      valid_stb_input = 1'b0;
-      ##5
-      valid_stb_input = 1'b1;
-      ##20;
+      //end of initialization
+
       
+      new_test(32'b01000010100000001101110100101111,
+               32'b01000011010000011110011001100110,
+               32'b01000011100000010010101001111111);
+
+      new_test(0,
+               0,
+               0);
+
+      new_test('1,
+               '1,
+               32'hff_c0_00_00);
+      
+      new_test(32'b01000010101111100000111100001111,
+               32'b01000011100001011101010101110100,
+               32'b01000011101101010101100100111000);
+
+      $display("======================== Tests passed ========================");
       $stop();
     end
 
 endmodule
+
