ef16e1ffe3a4e607c116c1977e28460d279c16bc
Fatyushkin D.V. <99551946+BlacksmithsHammer@users.noreply.github.com>
2024-08-13 13:56:20 +0400

    Delete ast_width_extender directory

--- ast_width_extender/tb/ast_we_generator.sv
+++
@@ -1,9 +0,0 @@
-class ast_we_generator #(
-  type T = ast_we_transaction,
-  parameter DATA_IN_W   = 64,
-  parameter EMPTY_IN_W  = $clog2(DATA_IN_W/8) ?  $clog2(DATA_IN_W/8) : 1,
-  parameter CHANNEL_W   = 10,
-  parameter DATA_OUT_W  = 256,
-  parameter EMPTY_OUT_W = $clog2(DATA_OUT_W/8) ?  $clog2(DATA_OUT_W/8) : 1
-);
-endclass
--- ast_width_extender/tb/ast_we_package.sv
+++
@@ -1,9 +0,0 @@
-package ast_we_package;
-
-  `include "macro.sv"
-  `include "ast_we_transaction.sv"
-  `include "ast_we_generator.sv"
-  `include "ast_we_driver.sv"
-  `include "ast_we_enviroment.sv"
-
-endpackage
